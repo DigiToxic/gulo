99095770bae3ee0df1379f748609090e9b9ea889
Digi | Toxic <DigiToxic55@gmail.com>
2023-03-12 17:21:32 -0400

    Official

--- pages/posts/About.tsx
+++ pages/posts/About.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from "next";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Marquee from "react-fast-marquee";
-import { motion } from "framer-motion";
 
 const About: NextPage = () => {
   return (
@@ -12,12 +11,7 @@ const About: NextPage = () => {
 
         <div className="bg-[url(https://static.wixstatic.com/media/a67ef4_bead6a0cac024db6a9177a6d4aa2152d~mv2.png)] h-[950px] bg-fixed bg-cover bg-no-repeat max-lg:h-[1200px] max-[785px]:h-[1300px] max-[660px]:h-[1400px] max-[540px]:h-[1500px] max-[470px]:h-[1600px]  max-[400px]:h-[1650px]">
           <div className="flex justify-evenly max-lg:flex-col">
-            <motion.div
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-              viewport={{ once: true }}
-              className="relative flex flex-col justify-center mt-6 max-lg:flex-row"
-            >
+            <div className="relative flex flex-col justify-center mt-6 max-lg:flex-row">
               <p className="flex flex-col justify-end text-center w-[600px] bg-[#191d05] bg-opacity-70 rounded-3xl pt-24 pb-4 px-4 mt-10 max-[1351px]:text-[14px] max-[1100px]:w-[500px] max-lg:w-5/6">
                 Gulo Valley is an NFT project focused on building utility
                 through art, community, lore and experience. Hidden deep within
@@ -39,14 +33,9 @@ const About: NextPage = () => {
                 src="/images/Border/1.png"
                 alt=""
               />
-            </motion.div>
+            </div>
 
-            <motion.div
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-              viewport={{ once: true }}
-              className="w-[50%] mt-16 border-[3px] border-[#34593A] rounded-3xl max-[1351px]:w-[40%] max-lg:w-full"
-            >
+            <div className="w-[50%] mt-16 border-[3px] border-[#34593A] rounded-3xl max-[1351px]:w-[40%] max-lg:w-full">
               <div className="overflow-x-hidden rounded-t-3xl">
                 <Marquee gradient={false} speed={200}>
                   <div className="ml-[60px] w-48 max-[1351px]:w-40 max-[1100px]:w-44 max-lg:w-40 max-[470px]:w-36">
@@ -210,7 +199,7 @@ const About: NextPage = () => {
                   </div>
                 </Marquee>
               </div>
-            </motion.div>
+            </div>
           </div>
         </div>
         <div className="-mt-[94px]">
